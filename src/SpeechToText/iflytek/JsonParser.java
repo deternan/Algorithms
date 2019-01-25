cf6a4efff0abab9f2c958eee33380d4e5cba386d
DELTA\BARRY.KE <barry.ke@deltaww.com>
2019-01-25 14:12:42 +0800

    add output function

--- src/SpeechToText/iflytek/JsonParser.java
+++ src/SpeechToText/iflytek/JsonParser.java
@@ -3,7 +3,7 @@ package SpeechToText.iflytek;
 /*
  * 
  * version: January 09, 2019 05:19 PM
- * Last revision: January 09, 2019 05:19 PM
+ * Last revision: January 25, 2019 02:11 PM
  * 
  * Author : Chao-Hsuan Ke
  * Institute: Delta Research Center
@@ -15,24 +15,34 @@ package SpeechToText.iflytek;
  */
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.OutputStreamWriter;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class JsonParser 
 {
+	// Read file
 	private String read_file_path = "";
 	private String read_file_name = "";
 	private BufferedReader bfr;
 	
 	private String Tag_onebest = "onebest";
 	
+	// Write output
+	private String output_folder = read_file_path;
+	private String output_file = "";
+	BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(output_folder + output_file), "utf-8"));
+	
 	public JsonParser() throws Exception
 	{
 		String Line = "";
 		FileReader fr = new FileReader(read_file_path + read_file_name);
 		BufferedReader bfr = new BufferedReader(fr);
+		
 		
 		Line = bfr.readLine();
 		//System.out.println(Line);		
@@ -45,10 +55,14 @@ public class JsonParser
 			JSONObject obj = new JSONObject(objArray.get(i).toString());
 			
 			System.out.println(obj.get(Tag_onebest));
+			
+			writer.write(obj.get(Tag_onebest)+"\n");
 		}
 		
 		bfr.close();
-	}
+		
+		writer.close();
+	}	
 	
 	public static void main(String args[])
 	{
